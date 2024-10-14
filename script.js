e2c5d63eeff971a347e3f85dfc944232a5ece8d0
Giovana Pollo <pollogiih2@gmail.comm>
2024-10-14 18:25:02 -0300

    alguns ajustes

--- script.js
+++ script.js
@@ -34,8 +34,10 @@ const styleTitle = document.querySelectorAll('.titulo-card')
 function estilizarTitulo() {
     styleTitle.forEach((title) => {
         title.style.color = 'rgb(43, 56, 91)';
-        title.textContent = 'Meu Card'
         title.style.padding = '18px'
+
+        //3
+        title.textContent = 'Meu Card'
     })
 }
 estilizarTitulo();
@@ -53,6 +55,8 @@ const styleDescription = document.querySelectorAll('.descricao-card')
 function estilizarDescricao() {
     styleDescription.forEach((description) => {
         description.style.padding = '10px';
+
+        //5
         description.textContent = 'Descrição modificada pelo JavaScript';
     })
 }
@@ -67,3 +71,55 @@ Ao clicar nesses elementos, deve chamar a função 'editarCard()'.
 Essa função deve mostrar um alerta com a mensagem "Clicou em
 Editar!".
 */
+
+const styleEditButton = document.querySelectorAll('.botao-editar')
+
+function estilizarBotaoEditar() {
+    styleEditButton.forEach((button) => {
+        button.style.backgroundColor = 'green';
+        button.style.color = 'white'
+        button.style.padding = '10px'
+        button.style.borderRadius = '15px'
+
+        //8
+        button.onclick = function editarCard() {
+            alert('Clicou em editar!')
+        }
+    })
+}
+estilizarBotaoEditar()
+
+/* 7. Capture, armazene e manipule todos os elementos de classe
+"botao-apagar". Modifique o estilo desses elementos para que fique
+semelhante a imagem. 
+
+9. Adicione o atributo 'onclick' nos elementos de classe
+“botão-apagar”. Ao clicar nesses elementos deve chamar a função
+'apagarCard()'. Essa função deve perguntar ao usuário se ele tem
+certeza da exclusão do card. Se o usuário confirmar a exclusão deve
+mostrar um alerta com a mensagem "Confirmado!", se não deve
+mostrar a mensagem "Cancelou!".
+*/
+
+const styleDeleteButton = document.querySelectorAll('.botao-apagar')
+
+function estilizarBotaoApagar() {
+    styleDeleteButton.forEach((button) => {
+        button.style.backgroundColor = 'red'
+        button.style.color = 'white'
+        button.style.padding = '10px'
+        button.style.borderRadius = '15px'
+
+        //9
+        button.onclick = function apagarCard() {
+            confirm('Atenção! Tem certeza que quer excluir esse card?')
+
+            if (confirm === true) {
+                alert('Confrimado!')
+            } else {
+                alert('Cancelou!')
+            }
+        }
+    })
+}
+estilizarBotaoApagar()
